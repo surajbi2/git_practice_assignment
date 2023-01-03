50603a56fdf53a21b13cf0407526dde1a84d2908
SURAJ KUMAR RAMAGIRI <ramagirisurajkumar@gmail.com>
2023-01-04 00:01:04 +0530

    prime number problem is done

---
+++ prime.js
@@ -0,0 +1,16 @@
+
+let count_a=0;
+let count_b=0;
+for(let i=1;i<=a;i++){
+    if(i%a==0){
+        count_a++;
+    } 
+    for(let j=1;j<=b;j++){
+        if(j%b==0){
+            count_b++
+        }
+    }
+    
+    }
+ (count_a==2&&count_b==2)? console.log("True"):console.log("false")
+       
